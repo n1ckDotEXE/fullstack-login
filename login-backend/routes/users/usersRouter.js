4f011b27f2e9a5d6605eb0e998647f886b0d26c7
n1ckDotEXE <admin@nicholasnam.com>
2021-03-25 02:54:14 -0400

    Adds routes to backend

---
+++ login-backend/routes/users/usersRouter.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+
+// Import functions from userController
+var {} = require("./controller/userController");
+
+// Imports functions from validator
+var {
+	checkIfEmptyMiddleware,
+	checkForSymbolMiddleware,
+	checkLoginIsEmpty,
+} = require("../lib/validator");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+	res.send("respond with a resource");
+});
+
+// Validate sign-up credentials, then add to database
+router.post(
+	"/sign-up",
+	checkIfEmptyMiddleware,
+	checkForSymbolMiddleware,
+	signUp
+);
+
+// Check if login fields are empty, then check valid login credentials
+router.post("/login", checkLoginIsEmpty, login);
+
+module.exports = router;
